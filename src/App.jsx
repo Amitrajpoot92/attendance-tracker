8f73fb31d2908668a008619de065029fcb5c6d95
Amitrajpoot92 <amitsrodz@gmail.com>
2026-07-05 15:18:27 +0530

    Fix Header import case sensitivity for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* Public Routes */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<home />} />
         <Route path="/register" element={<Register />} /> 
         <Route path="/login" element={<Login />} /> 
 
