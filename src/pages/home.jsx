f0e3c1d1124be530a6be728a6227b446b8048a50
Amitrajpoot92 <amitsrodz@gmail.com>
2026-07-05 15:14:12 +0530

    Fix Header import case sensitivity for Vercel

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import Header from "../components/header";
-import Footer from "../components/Footer";
+import header from "../components/header";
+import footer from "../components/footer";
 
 export default function Home() {
   return (
